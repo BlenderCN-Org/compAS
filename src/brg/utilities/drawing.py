b097cadd9d10b3aad7a5547b8eae1ea7661d3705
al <al@fortuna>
2016-10-21 09:19:56 +0200

    - Added mass_matrix to numerical.matrices. - Added create_matplotlib_axes in utilities.drawing.

--- src/brg/utilities/drawing.py
+++ src/brg/utilities/drawing.py
@@ -4,15 +4,63 @@ from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection, LineCollection
 
 
-__author__     = ['Tom Van Mele <vanmelet@ethz.ch>', ]
-__copyright__  = 'Copyright 2014, BLOCK Research Group - ETH Zurich'
+__author__     = ['Tom Van Mele <vanmelet@ethz.ch>',
+                  'Andrew Liew <liew@arch.ethz.ch>']
+__copyright__  = 'Copyright 2016, BLOCK Research Group - ETH Zurich'
 __license__    = 'MIT License'
 __version__    = '0.1'
-__date__       = 'Jul 21, 2015'
+__date__       = 'Oct 17, 2016'
+
+
+def create_matplotlib_axes(size=(10, 7),
+                           xlabel='$x$',
+                           ylabel='$y$',
+                           zlabel='$z$',
+                           fontname='Times New Roman',
+                           fontsize=20,
+                           grid=True,
+                           limits=None,
+                           ticklength=20,
+                           tickfontsize=15):
+    """Initialises plot axes object for matplotlib plotting.
+
+    Note:
+        z axes data included for future 3D plot implementation.
+
+    Parameters:
+        size (tuple): (horizontal, vertical) size of the figure.
+        xlabel (str): Label for the x-axis.
+        ylabel (str): Label for the y-axis.
+        zlabel (str): Label for the z-axis.
+        fontname (str): Fontname of the main labels and text.
+        fontsize (int): Fontsize of the main labels and text.
+        grid (boolean): Display grid.
+        limits (dic): Axis limits and tick spacing.
+        ticklength (float): length of the ticks.
+        tickfontsize (int): Fontsize of the ticks.
+
+    Returns:
+        object: Matplotlib axes.
+    """
+    if not limits:
+        limits = {'xmin': 0, 'dx': 0.1, 'xmax': 1,
+                  'ymin': 0, 'dy': 0.1, 'ymax': 1,
+                  'zmin': 0, 'dz': 0.1, 'zmax': 1}
+    plt.figure(facecolor='white', figsize=size)
+    plt.xlabel(xlabel, fontname=fontname, fontsize=fontsize)
+    plt.ylabel(ylabel, fontname=fontname, fontsize=fontsize)
+    if  grid:
+        plt.grid()
+    plt.minorticks_on()
+    plt.axis([limits['xmin'], limits['xmax'], limits['ymin'], limits['ymax']])
+    plt.tick_params(which='major', length=ticklength, labelsize=tickfontsize)
+    plt.tick_params(which='minor', length=ticklength*0.33)
+    axes = plt.gca()
+    return axes
 
 
 def draw_points():
-    pass
+    raise NotImplementedError
 
 
 def draw_points_in_matplotlib(points,
@@ -71,7 +119,7 @@ def draw_points_in_matplotlib(points,
 
 
 def draw_lines():
-    pass
+    raise NotImplementedError
 
 
 def draw_lines_in_matplotlib(lines,
@@ -91,7 +139,8 @@ def draw_lines_in_matplotlib(lines,
     for i in range(l):
         line = lines[i]
         lines.append(line)
-    collection = LineCollection(lines, linewidths=linewidth, colors=color, zorder=100)
+    collection = LineCollection(lines, linewidths=linewidth, colors=color,
+                                zorder=100)
     ax.add_collection(collection)
     if not axes:
         ax.set_xticks([])
