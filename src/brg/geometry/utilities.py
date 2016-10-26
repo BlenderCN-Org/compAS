dc98a502aef30b3f9d73c5902b480355758df4d9
al <al@fortuna>
2016-10-26 13:50:59 +0200

    - utilities.drawing 3D plot axes. - numerical.xforms docstrings.

---
+++ src/brg/geometry/utilities.py
@@ -0,0 +1,16 @@
+__author__     = ['Tom Van Mele <vanmelet@ethz.ch>', ]
+__copyright__  = 'Copyright 2014, BLOCK Research Group - ETH Zurich'
+__license__    = 'MIT License'
+__version__    = '0.1'
+__date__       = 'Oct 3, 2014'
+
+
+docs = []
+
+
+# ==============================================================================
+# Debugging
+# ==============================================================================
+
+if __name__ == '__main__':
+    pass
