860f014f57d716e52463aeb4ef555265deb81dc2
freeeki <freeeki@gmail.com>
2016-11-02 16:01:12 +0100

    Automated merge with bundle:/var/folders/q9/7tlpfl0s4dvbbj6k6rjcm9hw0000gn/T/SourceTreeTemp.MWvlek

--- examples/dynamic-relaxation_example.py
+++ examples/dynamic-relaxation_example.py
@@ -1,4 +1,4 @@
-""" An example of Dynamic Relaxation, using a Rhino model and subprocess.""" 
+""" An example of Dynamic Relaxation, using a Rhino model and subprocess."""
 
 from brg.datastructures.network.network import Network
 
@@ -7,7 +7,6 @@ from brg.datastructures.utilities import gkeys_from_network
 
 from brg_rhino.datastructures.mesh import RhinoMesh
 
-from brg_rhino.utilities.drawing import xdraw_lines
 from brg_rhino.utilities.drawing import xdraw_cylinders
 from brg_rhino.utilities.objects import get_point_coordinates
 from brg_rhino.utilities import wait
@@ -41,7 +40,7 @@ edges = [[mesh.vertex_coordinates(u), mesh.vertex_coordinates(v)]
 network = Network.from_lines(edges)
 network.set_dva(attr_dict={'bcx': 1, 'bcy': 1, 'bcz': 1,
                            'px': 0, 'py': 0, 'pz': -1})
-network.set_dea(attr_dict={'s0': 0.0*10**6, 'E': 1*10**9, 
+network.set_dea(attr_dict={'s0': 0.0*10**6, 'E': 1*10**9,
                            'A': 0.25*3.142*0.002**2})
 vkeys, ekeys = gkeys_from_network(network)
 
@@ -50,7 +49,7 @@ rs.CurrentLayer('lines')
 rs.DeleteObjects(rs.ObjectsByLayer('lines'))
 rs.EnableRedraw(False)
 for u, v in mesh.edges():
-    if  len(mesh.vertex_neighbours(u) + mesh.vertex_neighbours(v)) <= 6: 
+    if  len(mesh.vertex_neighbours(u) + mesh.vertex_neighbours(v)) <= 6:
         sp = mesh.vertex_coordinates(u)
         ep = mesh.vertex_coordinates(v)
         rs.AddLine(sp, ep)
@@ -72,12 +71,12 @@ for u, v in uv:
     network.set_edge_attribute(u, v, 's0', 0*10**6)
 
 # Settings
-settings={'tol': 1,
-          'steps': 50000,
-          'rtype': 'magnitude_1',
-          'factor': 1,
-          'ct': 't'}
-          
+settings = {'tol': 1,
+            'steps': 50000,
+            'rtype': 'magnitude_1',
+            'factor': 1,
+            'ct': 't'}
+
 # Subprocess
 sub = 'D:/py/dr_subprocess.py'
 ipath = 'D:/idata.json'
@@ -86,7 +85,8 @@ spath = 'D:/sdata.json'
 network.to_json(ipath)
 with open(spath, 'w+') as fp:
     json.dump(settings, fp)
-p = Popen(['pythonw', '-u', sub, ipath, opath, spath], stdout=PIPE, stderr=PIPE)
+p = Popen(['pythonw', '-u', sub, ipath, opath, spath],
+          stdout=PIPE, stderr=PIPE)
 while True:
     line = p.stdout.readline()
     if not line:
@@ -112,8 +112,8 @@ for u, v in network_.edges():
     r = abs(sc * network_.edge[u][v]['f'] / fmax)
     lines.append({'start': network_.vertex_coordinates(u),
                   'end': network_.vertex_coordinates(v),
-                  'name': 'edge_' + u + '-' + v, 
-                  'color': col, 
+                  'name': 'edge_' + u + '-' + v,
+                  'color': col,
                   'radius': r + rmin})
 xdraw_cylinders(lines)
 
