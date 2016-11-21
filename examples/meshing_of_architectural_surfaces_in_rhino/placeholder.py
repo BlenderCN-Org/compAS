ee40c22802f4e1aa8d77bb835627fac813296c1c
Rippmann <Rippmann@localhost>
2016-11-21 14:54:07 +0100

    example gridshell

--- examples/meshing_of_architectural_surfaces_in_rhino/placeholder.py
+++
@@ -1,5 +0,0 @@
-__author__     = ['Matthias Rippmann <rippmann@ethz.ch>', ]
-__copyright__  = 'Copyright 2016, Block Research Group - ETH Zurich'
-__license__    = 'MIT License'
-__version__    = '0.1'
-__date__       = 'Nov 11, 2016'
