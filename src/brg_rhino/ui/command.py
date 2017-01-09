fff16034328c8b51baa53dc59874ba1433bb25f0
Rippmann <Rippmann@localhost>
2017-01-09 17:59:25 +0100

    recursive function to create a rhino style command line menu - to generate command line menus in Rhino - features as many sub-level menus as you want - remembers selected options (saved in the file using setdocumentdata)
    doc string missing

--- src/brg_rhino/ui/command.py
+++ src/brg_rhino/ui/command.py
@@ -5,6 +5,7 @@ __copyright__  = 'Copyright 2016, BLOCK Research Group - ETH Zurich'
 __license__    = 'MIT License'
 __email__      = 'vanmelet@ethz.ch'
 
+import rhinoscriptsyntax as rs
 
 class Command(object):
 	pass
@@ -39,3 +40,50 @@ class CommandLoop(object):
 			self.handle_option()
 
 
+
+#rhino style command line menu
+def command_line_menu(interface):
+    new_options = []
+    sub_menus = {}
+    for option in interface["options"]:
+        if isinstance(option, basestring):
+            new_options.append(option)
+        else:
+            new_options.append(option["show"])
+            sub_menus[option["show"]] = option
+    result = rs.GetString(interface["message"],interface["default"],new_options)
+    interface["default"] = result
+    rs.SetDocumentData(interface["ID"],interface["ID"],result)
+    if result in sub_menus: result = command_line_menu(sub_menus[result])
+    return result
+    
+
+
+if __name__ == "__main__":
+
+    #template for command line menu
+
+	interface_3 =   {"options":["sub_sub_option_1","sub_sub_option_2"],
+	                "message":"Select C",
+	                "default":"sub_sub_option_1",
+	                "show":"sub_sub_menu",
+	                "ID":"interface_3"}
+	
+	interface_2 =   {"options":["sub_option_1",interface_3],
+	                "message":"Select B",
+	                "default":"sub_option_1",
+	                "show":"sub_menu",
+	                "ID":"interface_2"}
+	
+	interface_1 =   {"options":["option_1","option_2",interface_2],
+	                "message":"Select A",
+	                "default":"option_1",
+	                "show":None,
+	                "ID":"interface_1"}
+	
+	
+	if rs.GetDocumentData(interface_1["ID"],interface_1["ID"]): interface_1["default"] = rs.GetDocumentData(interface_1["ID"],interface_1["ID"])
+	if rs.GetDocumentData(interface_2["ID"],interface_2["ID"]): interface_2["default"] = rs.GetDocumentData(interface_2["ID"],interface_2["ID"])
+	if rs.GetDocumentData(interface_3["ID"],interface_3["ID"]): interface_3["default"] = rs.GetDocumentData(interface_3["ID"],interface_3["ID"])
+	
+	print command_line_menu(interface_1)
