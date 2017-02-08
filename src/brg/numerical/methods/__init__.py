709204f0677bb31aa652cef74d28c3a1de976797
Tom Van Mele <vanmelet@ethz.ch>
2017-02-08 21:21:44 +0100

    Working on the integration docs

---
+++ src/brg/numerical/methods/__init__.py
@@ -0,0 +1,52 @@
+"""
+.. _brg.numerical.methods:
+
+********************************************************************************
+methods
+********************************************************************************
+
+.. module:: brg.numerical.methods
+
+
+dynamic_relaxation
+==================
+
+.. currentmodule:: brg.numerical.methods.dynamic_relaxation
+
+:mod:`brg.numerical.methods.dynamic_relaxation`
+
+.. autosummary::
+    :toctree: generated/
+
+    rk1
+    rk2
+    rk3
+    rk4
+    dr
+
+
+force_density
+=============
+
+.. currentmodule:: brg.numerical.methods.force_density
+
+:mod:`brg.numerical.methods.force_density`
+
+.. autosummary::
+    :toctree: generated/
+
+    fd
+
+
+jiggle
+======
+
+.. currentmodule:: brg.numerical.methods.jiggle
+
+:mod:`brg.numerical.methods.jiggle`
+
+.. autosummary::
+    :toctree: generated/
+
+
+"""
