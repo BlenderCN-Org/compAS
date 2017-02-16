3f3dfd38e1e9dca5f9b94230c73d683c7dd8f2ba
Tom Van Mele <vanmelet@ethz.ch>
2017-02-16 17:07:27 +0100

    and now with proper docstring?

--- src/brg_rhino/conduits/labels.py
+++ src/brg_rhino/conduits/labels.py
@@ -2,9 +2,6 @@ from brg_rhino.conduits import Conduit
 
 try:
     from Rhino.Geometry import Point3d
-    from Rhino.Geometry import Line
-
-    from System.Collections.Generic import List
     from System.Drawing.Color import FromArgb
 
 except ImportError as e:
@@ -27,39 +24,37 @@ class LabelsConduit(Conduit):
     """A Rhino display conduit for labels.
 
     Parameters:
-        lines (list): A list of start-end point pairs that define the lines.
-        thickness (float): Optional.
-            The thickness of the conduit lines.
-            Default is ``1.0``.
+        labels (list): A list of label tuples. Each tuple contains a position and text for the label.
         color (tuple): Optional.
-            RGB color spec for the conduit lines.
+            RGB color spec for the dots.
             Default is ``None``.
 
     Example:
 
         .. code-block:: python
 
-            import random
+            from random import randint
             import time
 
-            points = [(1.0 * random.ranint(0, 30), 1.0 * random.randint(0, 30), 0.0) for _ in range(100)]
-            lines  = [(points[i], points[i + 1]) for i in range(99)]
-
-            conduit = LinesConduit(lines)
-            conduit.enable()
+            labels = [([1.0 * randint(0, 100), 1.0 * randint(0, 100), 0.0], str(i)) for i in range(100)]
 
             try:
+                conduit = LabelsConduit(labels)
+                conduit.Enabled = True
+
                 for i in range(100):
-                    points = [(1.0 * random.randint(0, 30), 1.0 * random.randint(0, 30), 0.0) for _ in range(100)]
-                    conduit.lines = [(points[i], points[i + 1]) for i in range(99)]
+                    labels = [([1.0 * randint(0, 100), 1.0 * randint(0, 100), 0.0], str(i)) for i in range(100)]
+                    conduit.labels = labels
+
                     conduit.redraw()
 
                     time.sleep(0.1)
-            except:
-                raise
+
+            except Exception as e:
+                print e
 
             finally:
-                conduit.disable()
+                conduit.Enabled = False
                 del conduit
 
     """
