daa2e77718883afa3d49d727d9c8e55d4e831e8e
rippmanm <rippmanm@localhost>
2017-02-23 12:42:17 +0100

    animation gif

--- src/compas/utilities/animation.py
+++ src/compas/utilities/animation.py
@@ -1,21 +1,32 @@
-import imageio
 import os
 
-def test():
-    print dir(imageio)
+try:
+    import imageio
+except ImportError:
+    pass
+#     import platform
+#     if platform.system() != 'Windows':
+#         raise
 
-def gif_from_images(path,name):
-    filenames = []
-#     for file in os.listdir(path):
-#         if file.endswith(".jpg"):
-#             print(path+file)
-#             filenames.append(path+file)
-#             
-#     
-#     with imageio.get_writer(path+name+'.gif', mode='I') as writer:
-#         for filename in filenames:
-#             image = imageio.imread(filename)
-#             writer.append_data(image)
+
+def gif_from_images(files, gif_path, fps=10, loop=0, reverse=False, pingpong=False, subrectangles=True):
+     
+    if reverse:
+        files.reverse()
+
+    if pingpong:
+        rev_files = files[:]
+        rev_files.reverse()
+        files += rev_files 
+     
+    with imageio.get_writer(gif_path, 
+                            mode = 'I', 
+                            fps = fps, 
+                            loop = loop, 
+                            subrectangles = subrectangles) as writer:
+        for filename in files:
+            image = imageio.imread(filename)
+            writer.append_data(image)
     
     
     
