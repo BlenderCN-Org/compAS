be1a41edb4e8f132cd7ce057780c306d479a74d0
al@fortuna <liew@arch.ethz.ch>
2017-03-15 16:40:14 +0100

    - DR and Blender stuff.

--- src/compas_blender/geometry/curve.py
+++ src/compas_blender/geometry/curve.py
@@ -1,36 +1,30 @@
 """compas_blender.geometry.curve : Manipulating Blender curves."""
 
-try:
-    import bpy
-    from mathutils.geometry import interpolate_bezier
-except ImportError:
-    pass
+from mathutils.geometry import interpolate_bezier
+
+import bpy
 
 
 __author__     = ['Andrew Liew <liew@arch.ethz.ch>']
-__copyright__  = 'Copyright 2016, BLOCK Research Group - ETH Zurich'
+__copyright__  = 'Copyright 2017, BLOCK Research Group - ETH Zurich'
 __license__    = 'MIT License'
-__date__       = 'Oct 17, 2016'
+__email__      = 'liew@arch.ethz.ch'
 
 
-def curve_to_mesh(curve):
-    """ Convert a curve object into a mesh edge.
-
-    Note:
-        A copy is created, original curve is not deleted.
+def bezier_curve_interpolate(curve, number=3):
+    """ Interpolate points along a Bezier curve.
 
     Parameters:
-        curve (obj): Curve object.
+        curve (obj): Bezier curve object.
+        number (int): Number of interpolation points.
 
     Returns:
-        obj: Resulting mesh object.
+        list: Interpolated points [x, y, z.]
     """
-    mesh = curve.to_mesh(bpy.context.scene, True, 'PREVIEW')
-    name = 'mesh_' + curve.name
-    mesh_object = bpy.data.objects.new(name, mesh)
-    mesh_object.location = curve.location
-    bpy.context.scene.objects.link(mesh_object)
-    return mesh_object
+    co, left, right = bezier_curve_points(curve)
+    vectors = interpolate_bezier(co[0], right[0], left[1], co[1], number)
+    points = [list(i) for i in vectors]
+    return points
 
 
 def bezier_curve_points(curve):
@@ -45,23 +39,45 @@ def bezier_curve_points(curve):
         list: Control point right handles.
     """
     points = curve.data.splines[0].bezier_points
-    co = [list(point.co) for point in points]
-    left = [list(point.handle_left) for point in points]
-    right = [list(point.handle_right) for point in points]
+    co = [list(i.co) for i in points]
+    left = [list(i.handle_left) for i in points]
+    right = [list(i.handle_right) for i in points]
     return co, left, right
 
 
-def bezier_curve_interpolate(curve, number):
-    """ Interpolate points along a Bezier curve.
+def curve_to_bmesh(curve, divisions=10, delete=False):
+    """ Convert a Blender curve object into a bmesh of edges.
 
     Parameters:
-        curve (obj): Bezier curve object.
-        number (int): Total number of interpolation points.
+        curve (obj): Curve object.
+        divisions (int): Number of divisions along length.
+        delete (bool): Delete original curve.
 
     Returns:
-        list: Interpolated points x, y, z.
+        obj: Resulting bmesh object.
     """
-    co, left, right = bezier_curve_points(curve)
-    vectors = interpolate_bezier(co[0], right[0], left[1], co[1], number)
-    points = [list(i) for i in vectors]
-    return points
+    curve.data.resolution_u = divisions
+    mesh = curve.to_mesh(bpy.context.scene, True, 'PREVIEW')
+    if delete:
+        curve.select = True
+        bpy.ops.object.delete()
+    name = 'bmesh_{0}'.format(curve.name)
+    bmesh = bpy.data.objects.new(name, mesh)
+    bmesh.location = [0, 0, 0]
+    bpy.context.scene.objects.link(bmesh)
+    return bmesh
+
+
+# ==============================================================================
+# Debugging
+# ==============================================================================
+
+if __name__ == "__main__":
+
+    from compas_blender.utilities.objects import get_objects_by_layer
+
+    curve = get_objects_by_layer(0)[0]
+    curve_to_bmesh(curve, divisions=3)
+    c, l, r = bezier_curve_points(curve)
+    points = bezier_curve_interpolate(curve, number=3)
+    print(points)
