2d82af0e1eb3c2e235224341c00d6fc85b85c982
rippmanm <rippmanm@localhost>
2017-03-16 10:00:40 +0100

    Merge with 4adfb0ea875a10569beb7eabd7578b56fb9621c1

--- src/compas/datastructures/mesh/mesh.py
+++ src/compas/datastructures/mesh/mesh.py
@@ -733,6 +733,18 @@ mesh summary
     def to_points(self, axes='xyz'):
         return [self.vertex_coordinates(key, axes) for key in self]
 
+    def to_vertices_and_faces(self):
+        """Return the vertices and faces of a mesh.
+
+        Returns:
+            (list, list): A tuple with a list of vertices, represented by their
+                XYZ coordinates, and a list of faces. Each face is a list of
+                indices referencing the list of vertex coordinates.
+        """
+        vertices = [self.vertex_coordinates(key) for key in self.vertex]
+        faces = [self.face_vertices(fkey, ordered=True) for fkey in self.face]
+        return vertices, faces
+
     # **************************************************************************
     # **************************************************************************
     # **************************************************************************
